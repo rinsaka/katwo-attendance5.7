a757c8f52dd902383e1a6ed55d9b91c43286518c
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2019-02-24 12:46:11 +0900

    make:seeder

---
+++ database/seeds/PartsTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PartsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
