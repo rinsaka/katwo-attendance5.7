fd0dd26792bef8149b0dc477ca1fca71067cfd4e
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2019-02-24 13:44:09 +0900

    simple test

--- tests/Feature/TopPageTest.php
+++ tests/Feature/TopPageTest.php
@@ -5,18 +5,75 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+use App\Professor;
+use App\Admin;
 
 class TopPageTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+  /**
+   * A basic feature test example.
+   *
+   * @return void
+   */
+  public function testExample()
+  {
+      $response = $this->get('/');
 
-        $response->assertStatus(200);
+      $response->assertStatus(200);
+  }
+
+  public function testWelcomePage()
+  {
+    $response = $this->get('/')
+                      ->assertSee('団員ログイン')
+                      ->assertSee('管理者ログイン');
+    $response->assertStatus(200);
+  }
+
+  public function testErrorPage()
+  {
+    $response = $this->get('/hoge');
+    $response->assertStatus(404);
+  }
+
+  public function testLoginPage()
+  {
+    // ログインページ
+    $response = $this->get('/login')
+                      ->assertSee('Login ID')
+                      ->assertSee('Password');
+    $response->assertStatus(200);
+  }
+
+  public function testLoginAsUser()
+  {
+
+    $user = User::where('id', 1)->first();
+
+    $response = $this->actingAs($user)
+                      ->get('/home/')
+                      ->assertSee('You are logged in!')
+                      ->assertSee('KatWO メンバー');
+  }
+
+  public function testAdminLoginPage()
+  {
+    // ログインページ
+    $response = $this->get('/admin/login')
+                      ->assertSee('Login ID')
+                      ->assertSee('Admin Login')
+                      ->assertSee('Password');
+    $response->assertStatus(200);
+  }
+  public function testLoginAsAdmin()
+    {
+      $admin = Admin::where('id', 1)->first();
+
+      $response = $this->actingAs($admin, 'admin')  // ガードを変更
+                        ->get('/admin/home/')
+                        ->assertSee('logged in as admin!')
+                        ->assertSee('KatWO 役員');
     }
+
 }
