13364306bd5da6dc5675be9c2ea2833a35f3dfb0
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2019-02-24 17:37:43 +0900

    routes

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,20 @@ Route::get('/', function () {
 });
 
 // Auth::routes();
-
+// User 認証不要
 Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('/login', 'Auth\LoginController@login');
 Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
 
 Route::group(['middleware' => 'auth:user'], function() {
   Route::get('/home', 'HomeController@index')->name('home');
+  Route::post('/home/confirm_delete', 'HomeController@confirm_delete');
+  Route::get('/home/{year}/{month}', 'HomeController@show');
+  Route::get('/home/{year}/{month}/create', 'HomeController@create');
+  Route::get('/home/{year}/{month}/{aid}/edit', 'HomeController@edit');
+  Route::post('/home/', 'HomeController@store')->name('store');
+  Route::patch('/home/', 'HomeController@update')->name('update');
+  Route::delete('/home/', 'HomeController@destroy')->name('destroy');
 });
 
 // Admin 認証不要
@@ -34,10 +41,10 @@ Route::group(['prefix' => 'admin'], function() {
 // Admin ログイン後
 Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function() {
   Route::get('home', 'Admin\HomeController@index')->name('admin.home');
-  // Route::get('activity/create', 'Admin\HomeController@create');
-  // Route::get('activity/{aid}', 'Admin\HomeController@edit')->name('admin.home');
-  // Route::patch('activity', 'Admin\HomeController@update')->name('admin_act_update');
-  // Route::post('activity', 'Admin\HomeController@store')->name('admin_act_store');
+  Route::get('activity/create', 'Admin\HomeController@create');
+  Route::get('activity/{aid}', 'Admin\HomeController@edit')->name('admin.home');
+  Route::patch('activity', 'Admin\HomeController@update')->name('admin_act_update');
+  Route::post('activity', 'Admin\HomeController@store')->name('admin_act_store');
   Route::post('logout', 'Admin\LoginController@logout')->name('admin.logout');
-  // Route::delete('activity/{id}', 'Admin\HomeController@destory');
+  Route::delete('activity/{id}', 'Admin\HomeController@destory');
 });
