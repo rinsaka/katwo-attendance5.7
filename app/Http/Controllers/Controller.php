49bc146ce4a67d9b611281d51c22a9c1e42df770
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2019-02-24 17:53:36 +0900

    controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,5 +9,14 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+  use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+  // 日付の文字列から曜日を返す
+  public function get_youbi($str_time)
+  {
+    $week = array( "日", "月", "火", "水", "木", "金", "土" );
+    $timestamp = strtotime($str_time);
+    $youbi = $week[date('w', $timestamp)];
+    return "(" . $youbi . ")";
+  }
 }
