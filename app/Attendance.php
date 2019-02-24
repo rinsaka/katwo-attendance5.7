6d5167ea79001f4dc086cdbebbb19cbc7906b051
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2019-02-24 17:22:43 +0900

    app/Attendance.php

--- app/Attendance.php
+++ app/Attendance.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Attendance extends Model
 {
-    //
+  // One to Many の Many 側
+  public function part()
+  {
+    return $this->belongsTo('App\Part');
+  }
+  // One to Many の Many 側
+  public function activity()
+  {
+    return $this->belongsTo('App\Activity');
+  }
 }
