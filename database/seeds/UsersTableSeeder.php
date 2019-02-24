115ca235c54b4b4dca641531ede070d3196649f6
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2019-02-24 11:11:09 +0900

    migration & seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,16 +1,27 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use Carbon\Carbon;
 
 class UsersTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    DB::table('users')->delete();
+
+    User::create([
+      'name' => 'KatWO メンバー',
+      'email' => 'a@sample.com',
+      'login_id' => env('USER_NAME'),
+      'password' => bcrypt(env('USER_PASSWORD')),
+      'created_at' => Carbon::now()
+    ]);
+
+  }
 }
